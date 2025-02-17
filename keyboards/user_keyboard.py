16b7577ae9c7886247559bb1daeb09a1927d831b
Александр Чехов <vladlabunets261@gmail.com>
2025-02-17 22:58:46 +0400

    inline keyboard sample

--- keyboards/user_keyboard.py
+++ keyboards/user_keyboard.py
@@ -1,6 +1,23 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def get_menu_keyboard():
     kb = [[KeyboardButton(text="/start"), KeyboardButton(text="/help")]]
     return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+
+
+def get_inline_keyboard():
+    # список кнопок
+    button_list: list = [2, 1]
+
+    # сами кнопки
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Первая кнопка", callback_data=f"FirstAction")
+    builder.button(text="Вторая кнопка", callback_data=f"SecondAction")
+    builder.button(text="Назад", callback_data="MainMenu")
+
+    # формируем расположение кнопок
+    builder.adjust(*button_list)
+
+    return builder.as_markup()
